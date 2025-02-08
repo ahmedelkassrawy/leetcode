84c17acec2471a8912c383e6f48b6171814aa1bf
Ahmed Elkassrawy <141945777+ahmedelkassrawy@users.noreply.github.com>
2025-02-08 15:03:50 +0200

    [26] [Time Beats: 100.00%] [Memory Beats: 50.89%] - LeetPush

--- 26-Remove-Duplicates-from-Sorted-Array.cpp
+++ 26-Remove-Duplicates-from-Sorted-Array.cpp
@@ -1,22 +1,19 @@
 class Solution {
 public:
-    int removeDuplicates(vector<int>& nums) {
-        if (nums.empty()) return 0;
+    int removeDuplicates(vector<int>& nums) 
+    {
+        if(nums.empty()) return -1;
 
         int l = 1;
-        for(int r = 1; r < nums.size();r++)
+        for(int r = 1; r <nums.size();r++)
         {
-            if(nums[r] != nums[r-1]) 
+            if(nums[r] != nums[r-1])
             {
                 nums[l] = nums[r];
                 l++;
             }
         }
-        return l;
 
-        ///[0,1,1,2] --> [0,1,2,_] 
-        //so we change it in place if the previous number is not equal to the 
-        //current number then we change the number at l to be the same number 
-        //at which the r is pointing not r - 1
+        return l;
     }
 };
