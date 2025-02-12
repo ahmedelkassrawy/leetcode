b598b8f176f67c6be089905c214b9d0684764919
Ahmed Elkassrawy <141945777+ahmedelkassrawy@users.noreply.github.com>
2025-02-12 16:45:19 +0200

    [217] [Time Beats: 5.24%] [Memory Beats: 5.86%] - LeetPush

---
+++ 217-Contains-Duplicate.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) 
+    {
+       set<int> st;
+       for(int i = 0; i < nums.size(); i++)
+       {
+            st.insert(nums[i]);
+       }
+
+       if(st.size() == nums.size())return false;
+       else return true;
+    }
+};
