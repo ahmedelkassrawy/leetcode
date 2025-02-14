29181f682740611d600d2c7954dbf769204c319a
Ahmed Elkassrawy <141945777+ahmedelkassrawy@users.noreply.github.com>
2025-02-14 13:31:13 +0200

    [3] [Time Beats: 24.40%] [Memory Beats: 37.58%] - LeetPush

--- 3-Longest-Substring-Without-Repeating-Characters.cpp
+++ 3-Longest-Substring-Without-Repeating-Characters.cpp
@@ -2,29 +2,24 @@ class Solution {
 public:
     int lengthOfLongestSubstring(string s) 
     {
-        int max_length = 0;         // To store the maximum length of substring
-        int l = 0;               // Left pointer of the sliding window
+        unordered_set<char> st;
 
-        // Use an unordered_set to store characters of the current window
-        std::unordered_set<char> char_set;
+        int l = 0;
+        int mx_len = 0;
 
-        // Iterate through the string with the right pointer
-        for (int r = 0; r < s.size(); ++r) 
+        for(int r = 0; r < s.size(); r++)
         {
-
-            // Shrink the window if the current character is already in the set
-            while (char_set.find(s[r]) != char_set.end()) {
-                char_set.erase(s[l]); // Remove the leftmost character
-                ++l;                  // Move the left pointer
+            while(st.find(s[r]) != st.end()) //repeated string
+            {
+                st.erase(s[l]);
+                l++;
             }
 
-            // Add the current character to the set
-            char_set.insert(s[r]);
+            st.insert(s[r]);
 
-            // Update the maximum length
-            max_length = std::max(max_length, r - l + 1);
+            mx_len = max(mx_len , r - l + 1);
         }
 
-        return max_length;
+        return mx_len;
     }
 };
