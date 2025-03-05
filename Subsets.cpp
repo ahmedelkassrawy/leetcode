0c270de2eba59859b0ee028c56221f70d0ea3dd4
Ahmed Elkassrawy <141945777+ahmedelkassrawy@users.noreply.github.com>
2025-03-05 22:00:39 +0200

    Update Subsets.cpp

--- Subsets.cpp
+++ Subsets.cpp
@@ -25,4 +25,33 @@ public:
         backtrack(nums,res,subst,0);
         return res;
     }
+
+
+///////////////////////////////////
+class Solution {
+public:
+    vector<vector<int>> subsets(vector<int>& nums) 
+    {
+        int n = nums.size();
+        int total = 1 << n;
+
+        vector<vector<int>> res;
+
+        for(int mask = 0;mask < total;mask++)
+        {
+            vector<int> subst;
+            for(int i = 0; i< n; i++)
+            {
+                if(mask & (1 << i))
+                {
+                    subst.push_back(nums[i]);
+                }
+            }
+            res.push_back(subst);
+        }
+
+        return res;
+
+    }
+};
 };
