603e7deff2c006916ee2fe807bdcaea709fc729f
Ahmed Elkassrawy <141945777+ahmedelkassrawy@users.noreply.github.com>
2025-07-06 14:47:54 +0300

    [1448] [Time Beats: 85.57%] [Memory Beats: 96.62%] - LeetPush

--- Count-Good-Nodes-in-Binary-Tree.cpp
+++ Count-Good-Nodes-in-Binary-Tree.cpp
@@ -1,32 +1,21 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
 public:
     int goodNodes(TreeNode* root) 
     {
-        return dfs(root, root -> val);
+        return dfs(root, root->val);
     }
 
-    int dfs(TreeNode* root,int curr_mx)
+    int dfs(TreeNode* root, int curr_mx)
     {
-        if(!root) return 0;
+        if (!root) return 0;
 
         int good = 0;
-        if(root -> val >= curr_mx) good = 1;
+        if (root->val >= curr_mx) good = 1;
 
-        curr_mx = max(curr_mx,root -> val);
+        curr_mx = max(curr_mx, root->val);  
 
-        good += dfs(root -> right,curr_mx);
-        good += dfs(root -> left,curr_mx);
+        good += dfs(root->left, curr_mx);
+        good += dfs(root->right, curr_mx);
 
         return good;
     }
